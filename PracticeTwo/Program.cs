e617c78369c5f78679b21c9e90a56fa7330c8a71
LUCASSANCHEZ12 <lucassanchez1@upb.edu>
2023-04-17 20:30:31 -0400

    6th commit, added new environments QA, UAT and  depedendcies SERILOG

--- PracticeTwo/Program.cs
+++ PracticeTwo/Program.cs
@@ -1,6 +1,16 @@
 using Microsoft.OpenApi.Models;
+using Serilog;
+
+
+//create the logger and setup your sinks, filters and properties
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Host.UseSerilog();
 
 // Add services to the container.
 builder.Services.AddControllers();
@@ -29,11 +39,9 @@ builder.Services.AddSwaggerGen(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
+
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
